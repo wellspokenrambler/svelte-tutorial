101a4c9c5b4a10308156e440643a11b80aa7dd9b
wellspokenrambler <36102623+wellspokenrambler@users.noreply.github.com>
2025-11-22 21:17:49 +0000

    restructured main site and routes to existing pages to accommodate rest of svelte tutorial and personal work

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,34 @@
 
 <h1>Isabelle May - Basic Svelte Tutorial Work Examples:</h1>
-<ol>
-	<li><a href="/1_introduction">Introduction</a></li>
-	<li><a href="/2_reactivity">Reactivity</a></li>
-	<li><a href="/3_props">Props</a></li>
-	<li><a href="/4_logic">Logic</a></li>
-	<li><a href="/5_events">Events</a></li>
-	<li><a href="/6_bindings">Bindings</a></li>
-	<li><a href="/7_classes_styles">Classes & Styles</a></li>
-	<li><a href="/8_actions">Actions</a></li>
-	<li><a href="/9_transitions">Transitions</a></li>
-</ol>
+<div class="flex w-full h-full flex-wrap">
+    <div class="w-full md:w-1/2 lg:w-1/4">
+        <h2>Basic Svelte</h2>
+        <ol>
+            <li><a href="/basic-svelte/1_introduction">Introduction</a></li>
+            <li><a href="/basic-svelte/2_reactivity">Reactivity</a></li>
+            <li><a href="/basic-svelte/3_props">Props</a></li>
+            <li><a href="/basic-svelte/4_logic">Logic</a></li>
+            <li><a href="/basic-svelte/5_events">Events</a></li>
+            <li><a href="/basic-svelte/6_bindings">Bindings</a></li>
+            <li><a href="/basic-svelte/7_classes_styles">Classes & Styles</a></li>
+            <li><a href="/basic-svelte/8_actions">Actions</a></li>
+            <li><a href="/basic-svelte/9_transitions">Transitions</a></li>
+        </ol>
+    </div>
+    <div class="w-full md:w-1/2 lg:w-1/4">
+        <h2>Advanced Svelte</h2>
+    </div>
+
+    <div class="w-full md:w-1/2 lg:w-1/4">
+        <h2>Basic Sveltekit</h2>
+    </div>
+
+    <div class="w-full md:w-1/2 lg:w-1/4">
+        <h2>Advanced Sveltekit</h2>
+    </div>
+
+    <div class="w-full">
+        <h2>Other Examples and Challenges</h2>
+    </div>
+</div>
+
