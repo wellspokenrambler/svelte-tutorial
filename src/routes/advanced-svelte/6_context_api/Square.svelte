666fad5cda13724c2c371ac86d75d28a47247ccb
wellspokenrambler <36102623+wellspokenrambler@users.noreply.github.com>
2026-01-11 10:24:43 +0000

    filled in context api page

---
+++ src/routes/advanced-svelte/6_context_api/Square.svelte
@@ -0,0 +1,19 @@
+<script>
+    import { getContext } from "svelte";
+
+    let {x,y,size,rotate} = $props();
+
+    getContext('canvas').addItem(draw);
+
+    function draw(context) {
+        context.save();
+
+        context.translate(x,y);
+        context.rotate(rotate);
+
+        context.strokeStyle = 'black';
+        context.strokeRect(-size/2,-size/2,size,size);
+
+        context.restore();
+    }
+</script>
