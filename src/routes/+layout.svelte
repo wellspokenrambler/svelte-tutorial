c11cb886002d10df976a2f8d8b83c2b2539322b5
wellspokenrambler <36102623+wellspokenrambler@users.noreply.github.com>
2026-03-01 14:03:20 +0000

    removed updated example

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -13,14 +13,5 @@
 </svelte:head>
 
 <a href="/" class="p-1 border-2 border-green-500 absolute top-0 left-0 z-10">Home</a>
-{#if updated.current !== updated.previous}
-	<div class="absolute bottom-0 right-0 p-4 flex flex-col gap-2 z-10">
-		<p class="text-sm">Updated to v.{version}!</p>
-		<button class="ml-2 px-2 py-1 bg-gray-200 rounded" onclick={() => location.reload()}>
-			Reload
-		</button>
-	</div>
-	{:else}
-	<p class="absolute bottom-0 right-0 p-4text-sm text-gray-500">v.{version}</p>
-{/if}
+<p class="absolute bottom-0 right-0 p-4 text-sm text-gray-500">v.{version}</p>
 {@render children()}
