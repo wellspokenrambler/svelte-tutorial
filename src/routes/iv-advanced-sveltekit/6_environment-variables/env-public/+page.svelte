467c86c41aa4c3e21c91b1af9a6b4859a348fb50
wellspokenrambler <36102623+wellspokenrambler@users.noreply.github.com>
2026-04-05 16:16:59 +0100

    adjusted spacing on public env variables page

--- src/routes/iv-advanced-sveltekit/6_environment-variables/env-public/+page.svelte
+++ src/routes/iv-advanced-sveltekit/6_environment-variables/env-public/+page.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <div class="absolute inset-0 -z-1" style="background-color: {PUBLIC_THEME_BACKGROUND_COLOR}; color: {PUBLIC_THEME_FOREGROUND_COLOR};">
-    <h1>Public Environment Variables</h1>
+    <h1 class="mt-4">Public Environment Variables</h1>
     <p>Background Color: {PUBLIC_THEME_BACKGROUND_COLOR}</p>
     <p>Foreground Color: {PUBLIC_THEME_FOREGROUND_COLOR}</p>
 </div>
